7b54abad7a0b5794d520ce94395ed116bb822185
Anureet Kaur Tiwana <68457535+AnureetKaurTiwana@users.noreply.github.com>
2021-04-30 12:58:33 +0530

    Add files via upload

---
+++ Draw_Grid_ForLoop.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+for i in range(0,3):
+    print('+'+'-'*3+'+'+'-'*3+'+')
+    if i<2:
+        for i in range(0,3):
+            print('|'+' '*3+'|'+' '*3+'|')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
