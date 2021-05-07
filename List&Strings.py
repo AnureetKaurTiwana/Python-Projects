50e956a5ffeb6aef9a1b4729ae8e61385d2fa987
Anureet Kaur Tiwana <68457535+AnureetKaurTiwana@users.noreply.github.com>
2021-05-07 16:47:58 +0530

    Add files via upload

---
+++ List&Strings.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#list method
+s='spam anu'
+t=list(s)
+print(t)
+
+
+# In[4]:
+
+
+#split method
+t=s.split()
+print(t)
+
+
+# In[7]:
+
+
+#delimiter
+s='spam-is-in-the computer'
+t=s.split('-')
+print(t)
+
+
+# In[10]:
+
+
+#join function
+delimiter='- '
+print(delimiter.join(t))
+
+
+# In[ ]:
+
+
+
+
